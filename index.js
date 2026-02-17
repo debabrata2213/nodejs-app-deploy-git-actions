ccdd7d01d94568127c70faa1761681737d5ddd0c
debabrata2213 <debabrata.mukherjee@cloudkaptan.com>
2026-02-17 16:36:07 +0530

    Changed response message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT ?? 8080;
 
 app.get('/',(req,res)=>{
-    return res.json({msg:"From the Server..."})
+    return res.json({msg:"From the Server on port 8080..."})
 })
 
 app.listen(port,()=>{
